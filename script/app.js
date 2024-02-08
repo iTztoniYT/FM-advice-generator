9fc4f8500e09b685def52309df9861873993fcb9
iTztoniYT <tonywayne903@gmail.com>
2024-02-08 16:57:51 -0400

    last commit with fixed button placement

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 //reference to the html
 const advice = document.querySelector('.advice-gen');
 const btn = document.querySelector('.button');
-
+const patternDivider = document.querySelector('.pattern-divider-img');
 
 
 const updateUI = (getQuotes) => {
@@ -11,7 +11,7 @@ const updateUI = (getQuotes) => {
     advice.innerHTML = `
         <p class="advice">advice #${quotes.slip.id}</p>
         <p class="quotes">${quotes.slip.advice}</p>
-        <img src="./images/pattern-divider-desktop.svg" alt="image of a divider">
+        <img src="${getPatternDividerSrc()}" alt="image of a divider under a quote" class="pattern-divider-img">
     `;
 };
 
@@ -28,6 +28,25 @@ const updateInitialQuote = () => {
             updateUI(data);
         });
 };
+
+const getPatternDividerSrc = () => {
+    // Get the window width
+    const windowWidth = window.innerWidth;
+
+    // Choose the appropriate image source based on window width
+    if (windowWidth < 375) {
+        console.log('works');
+        return "./images/pattern-divider-mobile.svg";
+    } else {
+        return "./images/pattern-divider-desktop.svg";
+    }
+};
+
+// Event listener for window resize
+window.addEventListener('resize', () => {
+    // Update the pattern divider image source on window resize
+    patternDivider.src = getPatternDividerSrc();
+});
 
 
 //create an eventlistener
